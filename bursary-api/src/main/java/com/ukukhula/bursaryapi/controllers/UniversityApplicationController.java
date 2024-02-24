bde90a9fcb20ebc1fb1365b1cc359d8b84876f0b
tebogomotibane <tebogomotibane@gmail.com>
2024-02-24 11:22:56 +0200

    modified controller after deleting service and repo implementation interface

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityApplicationController.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityApplicationController.java
@@ -3,7 +3,7 @@ package com.ukukhula.bursaryapi.controllers;
 import com.ukukhula.bursaryapi.assemblers.UniversityApplicationModelAssembler;
 import com.ukukhula.bursaryapi.entities.University;
 import com.ukukhula.bursaryapi.entities.UniversityApplication;
-import com.ukukhula.bursaryapi.services.UniversityApplicationService;
+import com.ukukhula.bursaryapi.repositories.UniversityApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,22 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UniversityApplicationController {
 
-    final
-    UniversityApplicationService universityApplicationService;
+    final UniversityApplicationRepository universityApplicationRepository;
 
     private final UniversityApplicationModelAssembler assembler;
 
-
-    public UniversityApplicationController(UniversityApplicationService universityApplicationService, UniversityApplicationModelAssembler assembler) {
-        this.universityApplicationService = universityApplicationService;
+    public UniversityApplicationController(UniversityApplicationRepository universityApplicationRepository,
+            UniversityApplicationModelAssembler assembler) {
+        this.universityApplicationRepository = universityApplicationRepository;
         this.assembler = assembler;
     }
 
     @GetMapping("/universities/application/{id}")
     public EntityModel<UniversityApplication> oneApplication(@PathVariable int id) {
 
-        UniversityApplication universityApplication =
-                universityApplicationService.getApplicationByUniversityId(id);
+        UniversityApplication universityApplication = universityApplicationRepository.getApplicationByUniversityId(id);
         return assembler.toModel(universityApplication);
     }
 }

0ee4ca9b3b8f1c727dd8f2fb9e1e4e32ecddda17
tebogomotibane <tebogomotibane@gmail.com>
2024-02-24 11:21:38 +0200

    removed university application service and repo implementation

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/services/UniversityApplicationService.java
+++
@@ -1,7 +0,0 @@
-package com.ukukhula.bursaryapi.services;
-
-import com.ukukhula.bursaryapi.entities.UniversityApplication;
-
-public interface UniversityApplicationService {
-    UniversityApplication getApplicationByUniversityId(int universityId);
-}

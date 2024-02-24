d740212987841cae02fc22c6faf53d4b77442097
tebogomotibane <tebogomotibane@gmail.com>
2024-02-24 12:08:58 +0200

    modified controller after deleting assembler, service and university implementation class

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityController.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/controllers/UniversityController.java
@@ -2,10 +2,10 @@ package com.ukukhula.bursaryapi.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ukukhula.bursaryapi.assemblers.UniversityModelAssembler;
 import com.ukukhula.bursaryapi.entities.University;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
@@ -15,44 +15,35 @@ import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.ukukhula.bursaryapi.services.UniversityService;
+import com.ukukhula.bursaryapi.repositories.UniversityRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class UniversityController {
 
-  private final UniversityService universityService;
+  private final UniversityRepository universityRepository;
 
-  private final UniversityModelAssembler assembler;
+  UniversityController(UniversityRepository universityRepository) {
 
-  UniversityController(UniversityModelAssembler assembler, UniversityService universityService) {
-    this.assembler = assembler;
-    this.universityService = universityService;
+    this.universityRepository = universityRepository;
   }
 
   @GetMapping("/universities")
-  public CollectionModel<EntityModel<University>> all() {
+  public List<University> all() {
 
-    List<EntityModel<University>> universities = universityService.getAllUniversities().stream()
-        .map(assembler::toModel)
-        .collect(Collectors.toList());
-
-    return CollectionModel.of(universities, linkTo(methodOn(UniversityController.class).all()).withSelfRel());
+    return universityRepository.getAllUniversities();
   }
 
   @PostMapping("/universities")
-  public ResponseEntity<?> newUniversity(@RequestBody University newUniversity) {
-    EntityModel<University> entityModel = assembler.toModel(universityService.addUniversity(newUniversity.getName()));
-
-    return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
-        .body(entityModel);
+  public Integer newUniversity(@RequestBody Map<String, Object> universityBodyObject) {
+    String name = String.valueOf(universityBodyObject.get("name"));
+    return universityRepository.addUniversity(name);
   }
 
   @GetMapping("/universities/{id}")
-  public EntityModel<University> one(@PathVariable int id) {
+  public University one(@PathVariable int id) {
+    return universityRepository.getUniversityById(id);
 
-    University university = universityService.getUniversityById(id);
-    return assembler.toModel(university);
   }
 }

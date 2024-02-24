ec1c40396a620f44704e08a485d301e25495f0df
tebogomotibane <tebogomotibane@gmail.com>
2024-02-24 12:39:49 +0200

    deleted user service and modified controller to use reporitory directly

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/services/UserRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.ukukhula.bursaryapi.services;
-
-import com.ukukhula.bursaryapi.entities.UserRole;
-
-import java.util.Optional;
-
-public interface UserRoleService {
-    String getByRoleId(int roleId);
-}

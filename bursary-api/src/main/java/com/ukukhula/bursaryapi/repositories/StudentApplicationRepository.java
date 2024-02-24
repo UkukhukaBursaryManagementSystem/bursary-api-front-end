73b4efd5eba9697342c0b557c39288dc9c4c9f49
tebogomotibane <tebogomotibane@gmail.com>
2024-02-24 10:07:03 +0200

    moved implementation logic to one class

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/StudentApplicationRepository.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/StudentApplicationRepository.java
@@ -1,14 +1,59 @@
 package com.ukukhula.bursaryapi.repositories;
 
- import com.ukukhula.bursaryapi.entities.StudentApplication;
+import com.ukukhula.bursaryapi.entities.StudentApplication;
 
 import java.util.List;
 
-public interface StudentApplicationRepository {
-    StudentApplication findByStudentID(int studentID);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
-    List<StudentApplication> getAllStudentsApplications();
+@Repository
+public class StudentApplicationRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-    Integer updateStudentsApplicationStatus(int studentID, String status);
-    Integer updateStudentsApplicationColumnValue(int studentID, String columnName, String value); //don't allow edit of status column unless status in review
+    public StudentApplicationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<StudentApplication> studentRowMapper = ((resultSet,
+            rowNumber) -> {
+        return new StudentApplication(resultSet.getInt("ID"),
+                resultSet.getInt("StudentID"),
+                resultSet.getString("Motivation"),
+                resultSet.getBigDecimal("BursaryAmount"),
+                resultSet.getString("StatusId"),
+                resultSet.getString("ReviewerComment"),
+                resultSet.getDate("Date"));
+    });
+
+    public StudentApplication findByStudentID(int studentID) {
+        String SQL = "SELECT * FROM StudentApplication WHERE StudentID = ?";
+
+        StudentApplication students = jdbcTemplate.queryForObject(SQL, studentRowMapper, studentID);
+        return students;
+    }
+
+    public List<StudentApplication> getAllStudentsApplications() {
+        final String SQL = "SELECT * FROM StudentApplication";
+
+        List<StudentApplication> students = jdbcTemplate.query(SQL, studentRowMapper);
+        return students;
+    }
+
+    public Integer updateStudentsApplicationStatus(int studentID, String status) {
+        final String SQL = "UPDATE StudentApplication SET Status = ? WHERE StudentID = ?";
+
+        return jdbcTemplate.update(SQL, status, studentID);
+    }
+
+    public Integer updateStudentsApplicationColumnValue(int studentID, String columnName, String value) {
+
+        final String SQL = "UPDATE StudentApplication SET " + columnName + " = ? WHERE StudentID = ?";
+
+        return jdbcTemplate.update(SQL, value, studentID);
+    }
+
 }

06183b4db5b6b84892aa9648ad15f54dc7f34e4b
tebogomotibane <tebogomotibane@gmail.com>
2024-02-26 14:47:21 +0200

    modified logic for even university allocation

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
@@ -18,12 +18,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class UniversityAllocationRepository {
-    final String SQL = "SELECT * FROM [dbo].[UniversityAllocationsView] WHERE UniversityName = ?";
+
     @Autowired
     JdbcTemplate jdbcTemplate;
+    final String SELECT_AVAILABLE_ADMIN_BALANCE = "SELECT dbo.CalculateRemainingAdminBalance(?)";
 
     public UniversityAllocation getUniversityAllocationByName(String universityName) {
-        List<UniversityAllocation> result = jdbcTemplate.query(SQL, UniversityAllocationRowMapper, universityName);
+        final String VIEW_ALLOCATIONS = "SELECT * FROM [dbo].[UniversityAllocationsView] WHERE UniversityName = ?";
+        List<UniversityAllocation> result = jdbcTemplate.query(VIEW_ALLOCATIONS, UniversityAllocationRowMapper,
+                universityName);
         return result.isEmpty() ? null : result.get(0);
     }
 
@@ -33,41 +36,31 @@ public class UniversityAllocationRepository {
     }
 
     public Integer allocateFundsToAllUniversities() {
-        String COUNT_APPROVED_UNIVERSITIES = "SELECT COUNT(Status) FROM UniversityApplication WHERE Status = 'Approved'";
-        String SELECT_ADMIN_BALANCE = "SELECT TotalAmount FROM BursaryDetails WHERE Year = YEAR(GETDATE())";
-        String UPDATE_ALL_UNIVERSITY_ALLOCATIONS = "UPDATE UniversityAllocation " +
-                "SET Amount = ? " +
-                "WHERE UniversityID IN ( " +
-                "    SELECT ua.UniversityID " +
-                "    FROM UniversityApplication ua " +
-                "    WHERE ua.Status = 'Approved' " +
-                ")";
+        String COUNT_APPROVED_UNIVERSITIES = "SELECT dbo.GetApprovedUniversityCount()";
+        String ALLOCATE_FUNDS_EVENLY = "{CALL InsertUniversityAllocations(?)}";
+  
 
+        Year year = Year.now();
+        int intYear = year.getValue();
+
+        Integer numberOfApprovedUniversities = jdbcTemplate.queryForObject(COUNT_APPROVED_UNIVERSITIES, Integer.class);
+        BigDecimal availableBalance = jdbcTemplate.queryForObject(SELECT_AVAILABLE_ADMIN_BALANCE, BigDecimal.class,
+                intYear);
         try {
-
-            BigDecimal availableBalance = jdbcTemplate.queryForObject(SELECT_ADMIN_BALANCE, BigDecimal.class);
-
-            if (availableBalance.compareTo(BigDecimal.ZERO) == 0) {
-                throw new RuntimeException("Admin balance is 0. No funds available for allocation.");
+            if (numberOfApprovedUniversities == 0) {
+                throw new IllegalStateException("No universities approved for funding yet");
             }
 
-            Integer numberOfApprovedUniversities = jdbcTemplate.queryForObject(COUNT_APPROVED_UNIVERSITIES,
-                    Integer.class);
-
-            BigDecimal amountPerUniversity = availableBalance.divide(BigDecimal.valueOf(numberOfApprovedUniversities),
-                    4,
-                    RoundingMode.HALF_UP);
-
-            return jdbcTemplate.update(UPDATE_ALL_UNIVERSITY_ALLOCATIONS, amountPerUniversity);
-        } catch (DataAccessException e) {
-
-            throw new RuntimeException("Error allocating funds to all universities", e);
+            if (availableBalance.compareTo(BigDecimal.ZERO) == 0) {
+                throw new IllegalStateException("Admin balance is 0. No funds available for allocation.");
+            }
+            return jdbcTemplate.update(ALLOCATE_FUNDS_EVENLY,2024);
         } catch (RuntimeException e) {
 
             throw e;
         } catch (Exception e) {
 
-            throw new RuntimeException("Unexpected error occurred while allocating funds to all universities", e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -77,21 +70,18 @@ public class UniversityAllocationRepository {
         String SELECT_UNIVERSITY_STATUS = "SELECT dbo.GetUniversityApplicationStatusByUniversityName(?)";
         String applicationStatus = jdbcTemplate.queryForObject(SELECT_UNIVERSITY_STATUS, String.class, universityName);
 
-        String SELECT_ADMIN_BALANCE = "SELECT dbo.GetAdminBalanceForYear(?)";
-        BigDecimal adminBalance = jdbcTemplate.queryForObject(SELECT_ADMIN_BALANCE, BigDecimal.class, year);
-
-        String GET_TOTAL_SPENT = "{CALL GetTotalUniversityAllocations(?)}";
-        BigDecimal totalSpent = jdbcTemplate.queryForObject(GET_TOTAL_SPENT, BigDecimal.class, year);
-        BigDecimal remainingBalance = adminBalance.subtract(totalSpent);
+        BigDecimal availableAdminBalance = jdbcTemplate.queryForObject(SELECT_AVAILABLE_ADMIN_BALANCE, BigDecimal.class,
+                year);
 
         try {
-            if (amount.compareTo(adminBalance) > 0 || amount.compareTo(remainingBalance) > 0) {
+            if (amount.compareTo(availableAdminBalance) > 0) {
                 throw new IllegalStateException("Insufficient admin balance");
             }
 
             if (!"approved".equalsIgnoreCase(applicationStatus)) {
                 throw new IllegalStateException("University not approved for funding");
             }
+
             return jdbcTemplate.update(INSERT_NEW_ALLOCATION, universityName, amount, year);
         } catch (Exception e) {
             throw new RuntimeException(e);

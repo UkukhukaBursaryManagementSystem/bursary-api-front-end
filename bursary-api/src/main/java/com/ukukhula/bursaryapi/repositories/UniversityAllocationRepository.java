81cc36913cf527b1a6304b9f7e2864d185c91d93
tebogomotibane <tebogomotibane@gmail.com>
2024-02-26 03:35:50 +0200

    added more logic for addding university

--- bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
+++ bursary-api/src/main/java/com/ukukhula/bursaryapi/repositories/UniversityAllocationRepository.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Year;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -29,35 +30,6 @@ public class UniversityAllocationRepository {
     public List<UniversityAllocation> getAllUniversityAllocations() {
         String UNIVERSITY_ALLOCATIONS_VIEW = "SELECT * FROM [dbo].[UniversityAllocationsView]";
         return jdbcTemplate.query(UNIVERSITY_ALLOCATIONS_VIEW, UniversityAllocationRowMapper);
-    }
-
-    public Integer allocateFundsToUniversity(int id, BigDecimal amount) {
-        String ALLOCATE_STORE_PROCEDURE = "{CALL AllocateFundsToUniversity(?, ?)}";
-        String GET_TOTAL_SPENT_PROCEDURE = "{CALL GetTotalAllocationByYear(?)}";
-        String SELECT_ADMIN_BALANCE = "SELECT TotalAmount FROM BursaryDetails WHERE Year = YEAR(GETDATE())";
-
-        BigDecimal adminBalance = jdbcTemplate.queryForObject(SELECT_ADMIN_BALANCE, BigDecimal.class);
-        int currentYear = Year.now().getValue();
-        BigDecimal totalAllocated = jdbcTemplate.queryForObject(GET_TOTAL_SPENT_PROCEDURE, BigDecimal.class,
-                currentYear);
-        BigDecimal remainingBalance = adminBalance.subtract(totalAllocated);
-
-        try {
-            if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-                throw new IllegalArgumentException("Allocated amount must be greater than 0.");
-            }
-            if (amount.compareTo(remainingBalance) > 0) {
-                throw new IllegalArgumentException("Insufficient balance, cannot allocate funds");
-            }
-            return jdbcTemplate.update(ALLOCATE_STORE_PROCEDURE, id, amount);
-        } catch (DataAccessException e) {
-            throw new RuntimeException("Error allocating funds to university with ID: " + id, e);
-        } catch (IllegalArgumentException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException("Unexpected error occurred while allocating funds to university with ID: " + id,
-                    e);
-        }
     }
 
     public Integer allocateFundsToAllUniversities() {
@@ -99,19 +71,30 @@ public class UniversityAllocationRepository {
         }
     }
 
-    public Integer addNewAllocation(int universityId, BigDecimal amount, int bursaryDetails) {
-        final String INSERT_NEW_ALLOCATION = "INSERT INTO UniversityAllocation (universityID,amount,BursaryDetailsID)" +
-                "VALUES(?,?,?)";
-        String SELECT_UNIVERSITY_STATUS = "SELECT Status FROM UniversityApplication  WHERE ID=?";
-        String status = jdbcTemplate.queryForObject(SELECT_UNIVERSITY_STATUS, String.class, universityId);
+    public Integer addNewAllocation(String universityName, BigDecimal amount, int year) {
+        final String INSERT_NEW_ALLOCATION = "{CALL InsertUniversityAllocation(?,?,?)}";
+
+        String SELECT_UNIVERSITY_STATUS = "SELECT dbo.GetUniversityApplicationStatusByUniversityName(?)";
+        String applicationStatus = jdbcTemplate.queryForObject(SELECT_UNIVERSITY_STATUS, String.class, universityName);
+
+        String SELECT_ADMIN_BALANCE = "SELECT dbo.GetAdminBalanceForYear(?)";
+        BigDecimal adminBalance = jdbcTemplate.queryForObject(SELECT_ADMIN_BALANCE, BigDecimal.class, year);
+
+        String GET_TOTAL_SPENT = "{CALL GetTotalUniversityAllocations(?)}";
+        BigDecimal totalSpent = jdbcTemplate.queryForObject(GET_TOTAL_SPENT, BigDecimal.class, year);
+        BigDecimal remainingBalance = adminBalance.subtract(totalSpent);
 
         try {
-            if (!"approved".equalsIgnoreCase(status)) {
+            if (amount.compareTo(adminBalance) > 0 || amount.compareTo(remainingBalance) > 0) {
+                throw new IllegalStateException("Insufficient admin balance");
+            }
+
+            if (!"approved".equalsIgnoreCase(applicationStatus)) {
                 throw new IllegalStateException("University not approved for funding");
             }
-            return jdbcTemplate.update(INSERT_NEW_ALLOCATION, universityId, amount, bursaryDetails);
+            return jdbcTemplate.update(INSERT_NEW_ALLOCATION, universityName, amount, year);
         } catch (Exception e) {
-            throw new RuntimeException("Error adding new allocation for university"); // more info
+            throw new RuntimeException(e);
         }
     }
 

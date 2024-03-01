ad5364231780e1356a0e6ed75a260504e55bae74
gaboneweb <bulelanigabonewe@gmail.com>
2024-03-01 22:48:14 +0200

    Admin and HOD endpoints

--- src/main/java/com/ukukhula/bursaryapi/controllers/UserController.java
+++ src/main/java/com/ukukhula/bursaryapi/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.ukukhula.bursaryapi.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ukukhula.bursaryapi.repositories.UserRepository;
+import com.ukukhula.bursaryapi.dto.AdminDTO;
+import com.ukukhula.bursaryapi.dto.HodDTO;
 import com.ukukhula.bursaryapi.entities.User;
 import org.springframework.http.ResponseEntity;
 
@@ -33,5 +35,29 @@ public class UserController
     public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
         User user = userRepository.getUserByEmail(email);
         return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+
+    @PostMapping("user/insert-admin")
+    public ResponseEntity<?> insertAdmin(@RequestBody AdminDTO adminDTO){
+        try{
+                userRepository.addAdmin(adminDTO);
+                return ResponseEntity.ok("{\"message\": \"The new admin "+adminDTO.getFirstName()+ " " +adminDTO.getLastName()+" was added successfully\"}");
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body("{\\\"error\\\": \\\"Failed to add new,please try again or check if the admin is not already in the system\"}");
+        }
+    }
+
+
+    @PostMapping("user/insert-hod")
+    public ResponseEntity<?> insertAdmin(@RequestBody HodDTO headOfDepartmentDTO){
+        try{
+
+            userRepository.addHod(headOfDepartmentDTO);
+            return ResponseEntity.ok("{\"message\": \"The new head of department "+headOfDepartmentDTO.getFirstName()+ " " +headOfDepartmentDTO.getLastName()+" was added successfully\"}");
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("{\\\"error\\\": \\\"Failed to add new head of department,please try again or check if the HOD is not already in the system\"}");
+        }
     }
 }

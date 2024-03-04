8228f6acdcc5ecad1c26ff6148a052041873239a
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2024-03-04 04:50:45 +0200

    Add token generation and token verifier: this will help with authorizing the requests without using spring security

--- src/main/java/com/ukukhula/bursaryapi/controllers/AuthController.java
+++ src/main/java/com/ukukhula/bursaryapi/controllers/AuthController.java
@@ -1,6 +1,10 @@
 package com.ukukhula.bursaryapi.controllers;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import java.util.Date;
 
 import com.ukukhula.bursaryapi.dto.AuthRequestDTO;
+import com.ukukhula.bursaryapi.dto.JwtResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,10 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthController {
 
-    @PostMapping("/auth/login")
 
-    public String GetToken(@RequestBody @Validated AuthRequestDTO authRequestDTO)
+
+    @PostMapping("/auth/login")
+    public JwtResponseDTO GetToken(@RequestBody @Validated AuthRequestDTO authRequestDTO)
     {
-            return "I have logged in";
+        String secretKey = "This is my key";
+        String issuer = "ukukhulaapi.azurewebsites.net";
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        String token = JWT.create()
+                .withSubject(authRequestDTO.getUsername())
+                .withIssuer(issuer)
+                .withAudience("3de2be63-d983-47bf-bd2f-92e7693477fa")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 14400000))
+                .sign(algorithm);
+        return JwtResponseDTO.builder().accessToken(token).build();
     }
 }
